ca2359acba816c334590fa94a750a6a902c92a74
Paddy Moran <pdmoran@outlook.com>
2017-06-26 16:56:24 +1200

    Added suer seeding

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'title' => 'Mr',
+            'first_name' => 'Thomas',
+            'middle_name' => 'David',
+            'surname' => 'Bloy',
+            'preferred_name' => 'Tom',
+            'email' => 'thomas@evolutionlawyers.nz',
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'title' => 'Ms',
+            'first_name' => 'Tamina',
+            'middle_name' => 'Kelly',
+            'surname' => 'Cunningham-Adams',
+            'email' => 'tamina@evolutionlawyers.nz',
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'title' => 'Mr',
+            'first_name' => 'Patrick',
+            'middle_name' => 'Daniel',
+            'surname' => 'Moran',
+            'email' => 'paddy@catalex.nz',
+            'preferred_name' => 'Paddy',
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'title' => 'Mr',
+            'first_name' => 'Joshua',
+            'middle_name' => 'Normand',
+            'surname' => 'Gagnon',
+            'email' => 'josh@catalex.nz',
+            'preferred_name' => 'Josh',
+            'password' => Hash::make('password')
+        ]);
+
+        User::create([
+            'title' => 'Ms',
+            'first_name' => 'Sarah',
+            'surname' => 'Thompson',
+            'email' => 'me@no-middle-name.nz',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
