170768dab3c6f9253cce0660b1af16075ae629a0
Paddy Moran <pdmoran@outlook.com>
2017-07-12 15:00:46 +1200

    Start login stuffs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,11 +10,4 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function __construct()
-    {
-        if (\App::environment('development')) {
-            \Auth::loginUsingId(1);
-        }
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,19 @@
 |
 */
 
-Route::get('{path?}', function () {
-    return view('index');
-})->where('path', '.*');
+// Authentication routes
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password reset routes
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+// Catch all and serve the main app
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('{path?}', function () {
+        return view('index');
+    })->where('path', '.*');
+});
