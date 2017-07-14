bdb629014de0eb1768eff5ded3ce4a4c4945c7e0
Paddy Moran <pdmoran@outlook.com>
2017-07-14 16:20:01 +1200

    Test for updating cpdpr records

--- tests/Feature/Controllers/ProfessionalDevelopmentControllerTest.php
+++ tests/Feature/Controllers/ProfessionalDevelopmentControllerTest.php
@@ -11,23 +11,17 @@ class ProfessionalDevelopmentControllerTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * A basic test example.
+     * Create record
      *
-     * @return void
+     * @test
      */
-    public function test_create_record()
+    public function create_record()
     {
         // Create a user
         $user = $this->createUser();
 
         // Data for new record
-        $data = [
-            'user_id'    => $user->id,
-            'date'       => '2017-11-16',
-            'minutes'    => 150,
-            'title'      => 'Testing',
-            'reflection' => 'One two three',
-        ];
+        $data = ['user_id' => $user->id, 'date' => '2017-11-16', 'minutes' => 150, 'title' => 'Testing', 'reflection' => 'One two three'];
 
         // Make the create POST request
         $response = $this->actingAs($user)->post('api/cpdpr', $data);
@@ -48,5 +42,39 @@ class ProfessionalDevelopmentControllerTest extends TestCase
 
         // Check the record was created for the correct user
         $this->assertEquals($data['user_id'], $record->user_id);
+    }
+
+    /**
+     * Update record
+     *
+     * @test
+     */
+    public function update_record()
+    {
+        // Create a user
+        $user = $this->createUser();
+
+        // Create the record
+        $record = ProfessionalDevelopmentRecord::forceCreate(['user_id' => $user->id, 'date' => '2017-11-16', 'minutes' => 150, 'title' => 'Testing', 'reflection' => 'One two three']);
+
+        // Make the update request
+        $updatedData = ['date' => '2014-03-01', 'minutes' => 10, 'title' => 'Updated', 'reflection' => 'a b c d e f'];
+        $response = $this->actingAs($user)->put('api/cpdpr/' . $record->id, $updatedData);
+
+        // Check the response status
+        $response->assertStatus(200);
+
+        // Get the response data and check it includes the updated message
+        $responseData = $response->getData(true);
+        $this->assertEquals($responseData['message'], 'CPDPR record updated.');
+
+        // Get the created record
+        $record = $record->fresh();
+
+        // Check the created record matches the data we sent the api endpoint
+        $this->assertTrue($this->arrayKeysMatch(['date', 'minutes', 'title', 'reflection'], $updatedData, $record->toArray()));
+
+        // Check the record was created for the correct user
+        $this->assertEquals($user->id, $record->user_id);
     }
 }
