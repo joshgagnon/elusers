f08306e856d23c016f96adf6cdbfc2066f393ca0
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-07-21 13:51:58 +1200

    fixed webpack settings for postcss

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,8 @@ module.exports = {
                         options: {
                             plugins: [
                                 autoprefixer
-                            ]
+                            ],
+                            sourceMap: true
                         }
                     },
                     {
