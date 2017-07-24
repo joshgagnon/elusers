b3d2b49ff3e64b2ad70fcc9a1de806c381732e50
Paddy Moran <pdmoran@outlook.com>
2017-07-25 00:11:16 +1200

    More work on create user form

--- resources/js/components/users/create.tsx
+++ resources/js/components/users/create.tsx
@@ -1,9 +1,16 @@
 import * as React from 'react';
 import PanelHOC from '../hoc/panelHoc';
+import { Form } from 'react-bootstrap';
+import { BasicDetailsFormFields } from '../my-profile/basicDetails';
 
 @PanelHOC('Create User')
+@reduxForm()
 export default class CreateUser extends React.PureComponent<EL.Propless, EL.Stateless> {
     render() {
-        return <h1>Create User</h1>;
+        return (
+            <Form>
+                <BasicDetailsFormFields />
+            </Form>
+        );
     }
 }
