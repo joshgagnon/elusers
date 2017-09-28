935e43b1c29d2f5da7f80c90159bdc9ce1332321
Paddy Moran <pdmoran@outlook.com>
2017-09-28 15:33:37 +1300

    Add models for client and deedfile

---
+++ app/Client.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Client extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = ['title', 'created_by_user_id'];
+
+    protected $visible = ['id', 'title', 'created_by_user_id'];
+
+    public static $validationRules = [
+        'title'              => 'required',
+        'created_by_user_id' => 'required|exists:users,id',
+    ];
+
+    /**
+     * Deed file relationship: a client has many deed files.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function deedFiles()
+    {
+        return $this->hasMany(DeedFile::class);
+    }
+
+    /**
+     * Created by relationship: a client was created by a user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function createdBy()
+    {
+        return $this->belongsTo(User::class, 'created_by_user_id');
+    }
+}
