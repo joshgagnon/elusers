c787cbc9f992959333e3c105e7000c5612d06f51
Paddy Moran <pdmoran@outlook.com>
2017-10-05 11:45:54 +1300

    Removed old clients query

--- resources/sql/clients.sql
+++
@@ -1,8 +0,0 @@
-SELECT id, title
-FROM clients
-
-WHERE
-    clients.created_by_user_id IN (SELECT id FROM users WHERE organisation_id = :org_id)
-    AND deleted_at IS NULL
-
-ORDER BY LOWER(title) ASC
