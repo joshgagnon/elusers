75e3a7a49e1cd25af8e0e8ac04e120ad40b59b73
Paddy Moran <pdmoran@outlook.com>
2017-10-10 10:40:50 +1300

    Make phone and email nullable for contacts

---
+++ database/migrations/2017_10_09_213852_make_email_and_phone_on_contacts_nullable.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeEmailAndPhoneOnContactsNullable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->string('email')->nullable()->change();
+            $table->string('phone')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->string('email')->nullable(false)->change();
+            $table->string('phone')->nullable(false)->change();
+        });
+    }
+}
