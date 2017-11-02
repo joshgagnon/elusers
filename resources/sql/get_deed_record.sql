5ecce705dca9baf5b2b0ad2c855fc98a38ec0407
Joshua Gagnon <josh.n.gagnon@gmail.com>
2017-11-03 12:25:42 +1300

    added file list to record query

--- resources/sql/get_deed_record.sql
+++ resources/sql/get_deed_record.sql
@@ -1,7 +1,15 @@
+WITH file_list AS (
+    SELECT array_agg(f.*)
+    FROM deed_packet_record_file dprf
+    JOIN files f on dprf.file_id = f.id
+    WHERE
+        dprf.deed_packet_record_id = :record_id
+)
+
 SELECT array_to_json(array_agg(row_to_json(q))) AS record
 FROM (
     SELECT
-        records.id, deed_packet_id, document_name, document_date, parties, matter_id, destruction_date, office_location_id, created_by_user_id, notes
+        records.id, deed_packet_id, document_name, document_date, parties, matter_id, destruction_date, office_location_id, created_by_user_id, notes, (SELECT * FROM file_list) as files
     FROM deed_packet_records records
     WHERE
         records.id = :record_id
