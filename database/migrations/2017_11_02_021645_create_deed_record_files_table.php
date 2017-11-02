d757a0d3d4128e6c4ffb47565f2b0759c1a73796
Paddy Moran <pdmoran@outlook.com>
2017-11-02 15:20:15 +1300

    created deed record file migration

---
+++ database/migrations/2017_11_02_021645_create_deed_record_files_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeedRecordFilesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('deed_record_files', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('deed_packet_record_id')->unsigned();
+            $table->foreign('deed_packet_record_id')->references('id')->on('deed_packet_records')->onDelete('cascade');
+
+            $table->text('path');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        //
+    }
+}
