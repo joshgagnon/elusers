3db67a2c591fe7b0d77114a9f19413cc3f013e0f
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-05-03 13:55:21 +1000

    finally importing jason formal

--- resources/js/components/templates/index.tsx
+++ resources/js/components/templates/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-
+import FormLoader from 'jasons-formal/lib/components/formLoader';
 
 export default class Templates extends React.PureComponent<any> {
     render() {
         return (
             <div>
-            indev
+                <FormLoader initialValues={{category: 'Evolution Templates', schema: 'letter'}} />
             </div>
         );
     }
--- resources/js/definitions.d.ts
+++ resources/js/definitions.d.ts
@@ -309,5 +309,7 @@ declare global {
             editable: boolean;
         }
     }
+
 }
+
 
