90be9956c6189c0139655258555f724932286506
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-05-15 15:07:15 +1000

    added context to forms

--- resources/js/components/templates/index.tsx
+++ resources/js/components/templates/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import { SimpleFormLoader as FormLoader } from 'jasons-formal/lib/components/formLoader';
 import Modals from 'jasons-formal/lib/components/modals';
+import { UsersHOC } from '../hoc/resourceHOCs';
+import Loading from '../loading';
 
+
+@UsersHOC()
 export default class Templates extends React.PureComponent<any> {
     render() {
+        if(!this.props.users.data){
+             return <Loading />
+        }
         return (
             <div>
-                <FormLoader initialValues={{category: 'Evolution Templates', schema: 'letter'}} />
+                <FormLoader initialValues={{category: 'Evolution Templates', schema: 'letter'}} context={{users: this.props.users.data.map(user => {
+                    return {value: user, title: `${user.firstName} ${user.surname}`}
+                })}} />
                 <Modals />
             </div>
         );
