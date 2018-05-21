939c8d70a15f963329322b3616b42dfab42897ac
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-05-21 15:03:00 +1000

    made new import contacts script

--- app/Console/Commands/ImportContacts.php
+++ app/Console/Commands/ImportContacts.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use App\Contact;
+use App\Address;
 use Illuminate\Console\Command;
 
 class ImportContacts extends Command
@@ -16,19 +17,66 @@ class ImportContacts extends Command
         $orgId = $this->argument('organisation_id');
         $handle = fopen(storage_path() . '/contacts.csv', 'r');
 
-        while ($csvLine = fgetcsv($handle, 1000, ",")) {
-            $name = $csvLine[0];
-            $email = $csvLine[1];
-            $phone = $csvLine[2];
+        while ($csvLine = fgetcsv($handle, $delimiter = ",")) {
+            //$this->info(join($csvLine, ', '));
+            //$this->info(count($csvLine));
+            if(count($csvLine) > 2){
+                $name = trim($csvLine[2]);
+                $type = $csvLine[10];
+                if($type == 'Company'){
+                    $fields = [
+                        'name' => $name,
+                        'type' => 'organisation',
+                        'metadata' => ['actionstepId' => $csvLine[0]],
+                        'email' => trim($csvLine[8]),
+                        'phone' => trim($csvLine[9]),
+                        'organisation_id' => $orgId
+                    ];
+                }
+                else{
+                    $names = explode('/ +/', trim($csvLine[2]));
+                    $middle = null;
+                    if(count($names) > 2){
+                        $middle = array_slice($names, 1, count($names) - 2);
+                        $middle = implode(' ', $middle);
+                    }
 
-            Contact::create([
-                'name' => $name,
-                'email' => $email,
-                'phone' => $phone,
-                'organisation_id' => $orgId,
-            ]);
+                    $fields = [
+                        'name' => $name,
+                        'first_name' => trim($names[0]),
+                        'middle_name' => $middle,
+                        'surname' => trim($names[count($names) - 1]),
+                        'type' => 'individual',
+                        'metadata' => ['actionstepId' => $csvLine[0]],
+                        'email' => trim($csvLine[8]),
+                        'phone' => trim($csvLine[9]),
+                        'organisation_id' => $orgId
+                    ];
+                }
+                $this->info(json_encode($fields, JSON_PRETTY_PRINT));
+                $contact = Contact::where('name', $name)->first();
+                if($contact){
+                    $contact->update($fields);
+                }
+                else{
+                    $contact = Contact::create($fields);
+                }
+                if($csvLine[3]){
+                    $contact->addresses()->delete();
+                    $address = [
+                        'address_one' => trim($csvLine[3]),
+                        'city' => trim($csvLine[4]),
+                        'post_code' => trim($csvLine[7]),
+                        'address_type' => 'postal',
+                        'country' => 'New Zealand'
+                    ];
+                    $contact->addresses()->create($address);
+                    $this->info(json_encode($address, JSON_PRETTY_PRINT));
 
-            $this->info($orgId . ' | Added ' . $name . ' (email: ' . $email . ', phone: ' . $phone . ')');
+                }
+
+            }
+
         }
     }
 }
