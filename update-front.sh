f6b7ad1e76402f0466dd804f07e8d84ce31ab88d
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-06-12 11:04:53 +1000

    added update script

---
+++ update-front.sh
@@ -0,0 +1,23 @@
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <server-username>"
+    exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+php artisan down
+
+sudo -u $1 git pull
+sudo -u $1 rm yarn.lock
+sudo -u $1 yarn install
+sudo -u $1 php artisan clear-compiled
+sudo -u $1 composer dump-autoload
+sudo -u $1 NODE_ENV=production node_modules/.bin/webpack
+sudo -u $1 php artisan migrate --force
+sudo -u $1 php artisan db:update-functions
+sudo -u $1 php artisan optimize
+
+php artisan up
