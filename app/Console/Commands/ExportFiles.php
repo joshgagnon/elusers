1db3b37f3e038fba0ed04f935a9c6bd6db17477e
Joshua Gagnon <josh.n.gagnon@gmail.com>
2019-01-14 16:56:05 +1100

    clearing folders on export

--- app/Console/Commands/ExportFiles.php
+++ app/Console/Commands/ExportFiles.php
@@ -12,6 +12,7 @@ use App\Address;
 use App\File;
 use Illuminate\Console\Command;
 
+
 class ExportFiles extends Command
 {
     protected $signature = 'export-files {organisation_id} ${output_dir}';
@@ -23,6 +24,21 @@ class ExportFiles extends Command
         parent::__construct();
     }
 
+    public function clear($path) {
+        $it = new \RecursiveDirectoryIterator($path, \RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new \RecursiveIteratorIterator($it,
+                     \RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($files as $file) {
+            if ($file->isDir()){
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
+        }
+        rmdir($path);
+    }
+
+
     public function contacts($org, $outputDir) {
         $contacts = Contact::where('organisation_id', $org->id)->with('contactable', 'files')->get();
 
@@ -33,6 +49,7 @@ class ExportFiles extends Command
                     continue;
                 }
                 $path = $outputDir.'/Contact Files/'.$contact->getName();
+                $this->clear($path);
                 $subpath = $file->getFullPath();
                 if($subpath) {
                     $path = $path.'/'.$subpath;
@@ -53,6 +70,7 @@ class ExportFiles extends Command
                     continue;
                 }
                 $path = $outputDir.'/Matter Files/'.$matter->matter_number;
+                $this->clear($path);
                 $subpath = $file->getFullPath();
                 if($subpath) {
                     $path = $path.'/'.$subpath;
@@ -66,15 +84,18 @@ class ExportFiles extends Command
 
     public function orgFiles($org, $outputDir) {
         $files = OrganisationFile::where('organisation_id', $org->id)->get();
+        $path = $outputDir.'/Organisation Files';
+        $this->clear($path);
+
         foreach($files as $file) {
             if($file->file->directory) {
                 continue;
             }
             $subpath = $file->file->getFullPath();
+            $path = $outputDir.'/Organisation Files';
             if($subpath) {
                 $path = $path.'/'.$subpath;
             }
-            $path = $outputDir.'/Organisation Files';
             @mkdir($path, 0777, true); // ignore result
             file_put_contents($path.'/'.$file->file->filename, $file->file->getContent($org->encryption_key));
             // put file into
@@ -87,6 +108,7 @@ class ExportFiles extends Command
     {
         $orgId = $this->argument('organisation_id');
         $outputDir = $this->argument('output_dir');
+
         $org = Organisation::find($orgId);
 
         $this->contacts($org, $outputDir);
