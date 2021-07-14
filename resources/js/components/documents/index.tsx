9cfe9b4e3844f2fbb438a1eadd9d6308e07f4c9c
Josh Gagnon <josh.n.gagnon@gmail.com>
2021-07-14 11:29:39 +1000

    fixed pdf versions

--- resources/js/components/documents/index.tsx
+++ resources/js/components/documents/index.tsx
@@ -1,27 +1,20 @@
 import * as React from 'react';
-import { connect } from 'react-redux';
-import PanelHOC from '../hoc/panelHOC';
-import { OrganisationDocumentsHOC, ContactDocumentsHOC, MatterDocumentsHOC } from '../hoc/resourceHOCs';
-import * as moment from 'moment';
-import { DocumentDropZone } from '../form-fields/documents';
-import { createNotification, createResource, deleteResource, confirmAction } from '../../actions';
-import { Form, ButtonToolbar, Button, Tabs, Tab, FormControl } from 'react-bootstrap';
-import { fullname, name } from '../utils';
-import { Link } from 'react-router';
-import { DocumentsTree } from 'components/documents/documentsTree';
-import { hasPermission } from '../utils/permissions';
+import {connect} from 'react-redux';
+import {OrganisationDocumentsHOC} from '../hoc/resourceHOCs';
+import {DocumentsTree} from 'components/documents/documentsTree';
+import {hasPermission} from '../utils/permissions';
 
 
-interface  DocumentsProps {
-      documents: EL.Resource<EL.OrganisationDocument[]>;
-      canUpdate: boolean;
+interface DocumentsProps {
+    documents: EL.Resource<EL.OrganisationDocument[]>;
+    canUpdate: boolean;
 }
 
 @(connect((state: EL.State) => ({
     canUpdate: hasPermission(state.user, 'manage organisation documents')
 })) as any)
 @OrganisationDocumentsHOC({cache: true})
-export default  class Documents extends React.PureComponent<DocumentsProps> {
+export default class Documents extends React.PureComponent<DocumentsProps> {
 
     render() {
         return <DocumentsTree
@@ -31,6 +24,6 @@ export default  class Documents extends React.PureComponent<DocumentsProps> {
             basePath={`organisation-files`}
             cached={this.props.documents.cached}
             permissionControls={true}
-            canUpdate={this.props.canUpdate} />
+            canUpdate={this.props.canUpdate}/>
     }
 }
